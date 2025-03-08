8373aa6709808835132f96317ea93c3a49c36d52
Beichen Huang <71347503+BeichenHuang@users.noreply.github.com>
2025-03-08 18:34:51 +0000

    Update DeepSeek_s1.sh

--- examples/DeepSeek_s1.sh
+++ examples/DeepSeek_s1.sh
@@ -5,6 +5,10 @@
 
 save_dir="$1"
 
+echo "This script contain the quantization and basic experiments for DeepSeek-s1 as in Table 3"
+echo "Evaluation includes Wikitext2 perplexity, zero-shot evaluation with HellaSwag, Lambada and PIQA "
+echo "Estimated running time: 1 hr"
+echo "Estimated disk space: 9 GB"
 echo "===== MiLo Quantization ====="
 python utils/MiLo_quant_main.py --base_dir ${save_dir} --model_id DeepSeek --dense_rank 800 
 echo "===== Wikietxt2 PPL eval ====="
@@ -12,6 +16,3 @@ python utils/MiLo_eval_wikitext2_ppl.py --base_dir ${save_dir} --model_id DeepSe
 echo "===== Zero-Shot eval ====="
 python utils/MiLo_eval_zeroshot.py --base_dir ${save_dir} --model_id DeepSeek
 
-#optional running fewshots, since MMLU and TriQA take long time
-# echo "===== Few-Shots eval ====="
-# python MiLo_eval_fewshot.py --base_dr ${save_dir} --model DeepSeek
